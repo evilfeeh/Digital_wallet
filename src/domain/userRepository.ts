1c39b18b4a5614d19af696f25a5fa68515aee3c8
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2023-12-20 21:47:00 -0300

    feat: implements userRepository

---
+++ src/domain/userRepository.ts
@@ -0,0 +1,58 @@
+import { IUserRepository } from '../interfaces/user-repository'
+import { DataSource } from 'typeorm'
+import { User } from '../model/user'
+
+export class UserRepository implements IUserRepository {
+  AppDataSource: DataSource
+  constructor () {
+    this.AppDataSource = new DataSource({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE
+  })
+  }
+  async get(email: User['email']): Promise<User> {
+    return this.AppDataSource
+    .getRepository(User)
+    .createQueryBuilder('user')
+    .where('user.email = :email', { email })
+    .getOne()
+  }
+  async getAll(): Promise<User[]> {
+    return this.AppDataSource
+    .getRepository(User)
+    .createQueryBuilder('user')
+    .getMany()
+  }
+  async save(user: User): Promise<boolean> {
+    const { raw } = await this.AppDataSource
+    .createQueryBuilder()
+    .insert()
+    .into(User)
+    .values(user)
+    .execute()
+    return (raw.affected) ? true : false
+  }
+  async update(id: User['id'], toUpdate: any): Promise<boolean> {
+    const { affected } = await this.AppDataSource
+    .createQueryBuilder()
+    .update(User)
+    .set(toUpdate)
+    .where("id = :id", { id })
+    .execute()
+    return (affected) ? true : false
+  }
+  async delete(id: User['id']): Promise<boolean> {
+    const { affected } = await this.AppDataSource
+    .getRepository(User)
+    .createQueryBuilder('user')
+    .delete()
+    .from(User)
+    .where("id = :id", { id })
+    .execute()
+    return (affected) ? true : false
+  }
+}
