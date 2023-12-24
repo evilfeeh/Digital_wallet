30d2fa14b4b6e5aab412579f5b82e4a234c925e6
felipe goncalves <felipe.goncalves@semantix.ai>
2023-12-24 14:51:11 -0300

    refactor: remove validate

--- src/controllers/UserManagment.ts
+++ src/controllers/UserManagment.ts
@@ -6,7 +6,6 @@ import { UserRepository } from '../domain/userRepository'
 export default class UserManagment {
   private readonly dataValidation = new DataValidation()
   private readonly userRepository = new UserRepository()
-  private readonly password: string
   user: Iuser
   salt: string
   hash: string
@@ -14,14 +13,10 @@ export default class UserManagment {
   constructor (user: Iuser, password: string) {
     this.user = user
     this.user.active = true
-    this.password = password
     this.user.commonUser = this.isCommonUser();
     [this.salt, this.hash] = hashingPassword(password).split(':')
   }
   async new () {
-    const { status, message } = this.dataValidation.newUser(this.user, this.password)
-    if (status == 'Error') return { status, message }
-
     const clientExists = await this.userRepository.get(this.user.email)
     if (clientExists) return { stauts: 'Error', message: 'Email already registered' }
 
