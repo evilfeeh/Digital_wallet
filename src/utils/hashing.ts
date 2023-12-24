0edcd3a855a91318a22599ae5d5989b509acdd3b
felipe goncalves <felipe.goncalves@semantix.ai>
2023-12-24 15:19:39 -0300

    feat: add method to validate password

--- src/utils/hashing.ts
+++ src/utils/hashing.ts
@@ -1,7 +1,14 @@
-import { scryptSync, randomBytes, timingSafeEqual } from 'crypto'
+import { scryptSync, randomBytes, timingSafeEqual, BinaryLike } from 'crypto'
 
 export function hashingPassword (password: string) {
   const salt = randomBytes(16).toString('hex');
   const hashedPassword = scryptSync(password, salt, 64).toString('hex');
   return `${salt}:${hashedPassword}`
+}
+
+export function IsPasswordValid (passwordUnderValidation: BinaryLike, correctedHash: { split: (arg0: string) => [string, string]; }) {
+  const [salt, key] = correctedHash.split(':');
+  const hashedPassword = Buffer.from(scryptSync(passwordUnderValidation, salt, 64).toString('hex'));
+  const keyBuffer = Buffer.from(key, 'hex');
+  return timingSafeEqual(keyBuffer, hashedPassword);
 }
