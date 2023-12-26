958fcbcf3c0d33c7e1e057e141c2f67717ba702b
felipe goncalves <felipe.goncalves@semantix.ai>
2023-12-26 16:46:01 -0300

    feat: separe databases configuration

--- src/config/databases.ts
+++ src/config/databases.ts
@@ -1,4 +1,4 @@
-export const database_config = {
+export const postgres = {
   postgres: {
     type: 'postgres',
     host: process.env.DB_HOST,
@@ -6,15 +6,15 @@ export const database_config = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
-  },
-  mysql: {
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: 3306,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
   }
+}
+export const mysql = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 }
 
 export const mongodb = {
