30d9a7802dbbbbeff55ddfe08eba1f4c219cf3e9
evilfeeh <felipegoncalvesdsantos@gmail.com>
2024-01-03 18:53:45 -0300

    feat: validate payer, payee and amount

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import UserManagment from './services/UserManagment'
 import DataValidation from './utils/DataValidation'
+import { UserRepository } from './model/userRepository'
 
 class Index {
   dataValidation = new DataValidation
+  userRepository = new UserRepository
   createNewUser (user: any) {
     let userValidated = this.dataValidation.user(user)
     if (userValidated.status == 'Error') return userValidated
@@ -12,6 +14,20 @@ class Index {
 
     const userManagment = new UserManagment(user, user.password)
     return userManagment.new()
+  }
+
+  async addCashToUser (payer: any, payee: any, amount: number) {
+    const payerUser = await this.userRepository.get(payer)
+    const payeeUser = await this.userRepository.get(payee)
+
+    let payerValidated = this.dataValidation.user(payerUser)
+    if (payerValidated.status == 'Error') return payerValidated
+
+    let payeeValidated = this.dataValidation.user(payeeUser)
+    if (payeeValidated.status == 'Error') return payeeValidated
+
+    let cashValidated = this.dataValidation.cash(amount)
+    if (cashValidated.status == 'Error') return cashValidated
   }
 }
 
