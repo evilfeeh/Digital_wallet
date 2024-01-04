ef153323fabb2e012029c28311d979dcee96ab28
evilfeeh <felipegoncalvesdsantos@gmail.com>
2024-01-04 12:59:09 -0300

    fix: passing wallet to consult value

--- src/interfaces/wallet-repository.ts
+++ src/interfaces/wallet-repository.ts
@@ -3,7 +3,7 @@ import { Iwallet } from '../interfaces/wallet'
 export interface IWalletRepository {
   get: (walletId: Iwallet['user_id']) => Promise<Iwallet>;
   save: (wallet: Iwallet) => Promise<boolean>;
-  consult: () => Promise<number>;
+  consult: (wallet: Iwallet) => Promise<number>;
   deposit: (amount: number) => Promise<boolean>;
   withdraw: (amount: number) => Promise<boolean>;
   transfer: (amount: string) => Promise<boolean>;
