0fa0f74296250d186ac667eff0cefb3c8831494d
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2024-01-07 20:57:29 -0300

    refactor: simplifying User class

--- src/services/User.ts
+++ src/services/User.ts
@@ -1,5 +1,4 @@
 import { Iuser } from '../interfaces/user'
-import { IsPasswordValid } from '../utils/hashing'
 import { UserRepository } from '../model/userRepository'
 
 export class User {
@@ -8,46 +7,37 @@ export class User {
   salt: string
   hash: string
 
-  constructor (user: Iuser) {
-    this.user = user
-    this.user.active = true
-    this.user.commonUser = this.isCommonUser();
-  }
-
   async create (user: Iuser) {
+    user.active = true
+    user.commonUser = this.isCommonUser(user.CPF_CNPJ);
     const clientExists = await this.userRepository.get(user.email)
     if (clientExists) return false
 
     const insertedObj = await this.userRepository.save(user)
     if (!insertedObj) return false
-
     return true
   }
 
-  private isCommonUser () {
-    const document = this.user.CPF_CNPJ.replace(/.\/-/g, '');
+  private isCommonUser (cpfcnpj: string) {
+    const document = cpfcnpj.replace(/.\/-/g, '');
     return document.length === 11 ? true : false;
   }
 
   async get (email: string) {
     const user = await this.userRepository.get(email)
-    if (!user) return { status: 'Error', message: 'User or password invalid' }
-
-    const isPasswordValid = IsPasswordValid(user.hashPassword, this.user.hashPassword)
-    if (!isPasswordValid) return { status: 'Error', message: 'Invalid Password' }
-
+    if (!user) return false
     return user
   }
+
   async update (email: string, toUpdate: any) {
     const updated = await this.userRepository.update(email, toUpdate)
     if (!updated) return false
-
     return true
   }
+
   async delete (email: string) {
     const deleted = await this.userRepository.delete(email)
     if (!deleted) false
-
     return true
   }
 }
