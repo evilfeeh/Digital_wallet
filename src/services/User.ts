4273bb4384714f434d97ebc1b850afb993f9d5e3
evilfeeh <felipegoncalvesdsantos@gmail.com>
2024-01-07 20:04:14 -0300

    refactor: simplifying User Class

--- src/services/User.ts
+++ src/services/User.ts
@@ -1,52 +1,53 @@
 import { Iuser } from '../interfaces/user'
-import { hashingPassword, IsPasswordValid } from '../utils/hashing'
+import { IsPasswordValid } from '../utils/hashing'
 import { UserRepository } from '../model/userRepository'
 
-export default class User {
+export class User {
   private readonly userRepository = new UserRepository()
   user: Iuser
   salt: string
   hash: string
 
-  constructor (user: Iuser, password: string) {
+  constructor (user: Iuser) {
     this.user = user
     this.user.active = true
     this.user.commonUser = this.isCommonUser();
-    this.user.hashPassword = hashingPassword(password)
-    this.create()
   }
-  private async create () {
-    const clientExists = await this.userRepository.get(this.user.email)
-    if (clientExists) return { stauts: 'Error', message: 'Email already registered' }
 
-    const insertedObj = await this.userRepository.save(this.user)
-    if (!insertedObj) return { status: 'Error', message: 'Failed to insert User'}
+  async create (user: Iuser) {
+    const clientExists = await this.userRepository.get(user.email)
+    if (clientExists) return false
 
-    return { status: 'Sucess', message: 'User Created'}
+    const insertedObj = await this.userRepository.save(user)
+    if (!insertedObj) return false
+
+    return true
   }
+
   private isCommonUser () {
     const document = this.user.CPF_CNPJ.replace(/.\/-/g, '');
     return document.length === 11 ? true : false;
   }
-  async get () {
-    const user = await this.userRepository.get(this.user.email)
+
+  async get (email: string) {
+    const user = await this.userRepository.get(email)
     if (!user) return { status: 'Error', message: 'User or password invalid' }
 
     const isPasswordValid = IsPasswordValid(user.hashPassword, this.user.hashPassword)
     if (!isPasswordValid) return { status: 'Error', message: 'Invalid Password' }
 
-    return { status: 'Success', message: 'User Found', value: user  }
+    return user
   }
   async update (email: string, toUpdate: any) {
     const updated = await this.userRepository.update(email, toUpdate)
-    if (!updated) return { status: 'Error', message: 'Failed to update user' }
+    if (!updated) return false
 
-    return { status: 'Success', message: 'User Updated' }
+    return true
   }
   async delete (email: string) {
     const deleted = await this.userRepository.delete(email)
-    if (!deleted) return { status: 'Error', message: 'Failed to delete user' }
+    if (!deleted) false
 
-    return { status: 'Success', message: 'User Deleted' }
+    return true
   }
 }
