2041460c29b7fc60fb72f1d8cf7c3b16fd9648f3
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2024-01-07 23:08:25 -0300

    feat: add factory to implements user

---
+++ src/utils/buildUser.ts
@@ -0,0 +1,21 @@
+import { User, Wallet } from '../services'
+
+export async function buildUser (user_id: string) {
+  const user = await new User().get(user_id)
+  const wallet = await new Wallet().get(user_id)
+  const debit_amount = wallet.debit_amount
+  async function deposit () {
+    return this.wallet.deposit(this.order.payment_amount, user.id)
+  }
+  async function withdraw() {
+    return this.wallet.withdraw(this.order.payment_amount, user_id)
+  }
+
+  return {
+    user,
+    wallet,
+    debit_amount,
+    deposit,
+    withdraw
+  }
+}
