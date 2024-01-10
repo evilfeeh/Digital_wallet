fefe1b006dd9d60e881d74353fdc4a8b4385c041
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2024-01-09 22:15:23 -0300

    refactor: remove builder

--- src/utils/UserBuilder.ts
+++ src/utils/UserBuilder.ts
@@ -1,9 +1,11 @@
-import { User } from '../services'
+import { User, Wallet } from '../services'
 import { Iuser } from '../interfaces/user'
 import { hashingPassword, DataValidation } from './'
 
 export class UserBuilder {
   user: Iuser
+  userManagment = new User()
+  walletManagment = new Wallet()
   dataValidation = new DataValidation
   constructor() {
     this.user.fullname = ''
@@ -16,26 +18,36 @@ export class UserBuilder {
   }
 
   fullname(name: string) {
+    const { status, message } = this.dataValidation.checkFullname(name)
+    if (status === 'Error') throw new Error(message)
     this.user.fullname = name
     return this
   }
 
   cpfCnpj(CPF_CNPJ: string) {
+    const { status, message } = this.dataValidation.checkDocument(CPF_CNPJ)
+    if (status === 'Error') throw new Error(message)
     this.user.CPF_CNPJ = CPF_CNPJ
     return this
   }
 
   password(password: string) {
+    const { status, message } = this.dataValidation.checkPassword(password)
+    if (status === 'Error') throw new Error(message)
     this.user.hash = hashingPassword(password)
     return this
   }
 
   email(email: string) {
+    const { status, message } = this.dataValidation.checkEmail(email)
+    if (status === 'Error') throw new Error(message)
     this.user.email = email
     return  this
   }
 
   phone(phone: string) {
+    const { status, message } = this.dataValidation.checkPhone(phone)
+    if (status === 'Error') throw new Error(message)
     this.user.phone = phone
     return  this
   }
@@ -47,8 +59,11 @@ export class UserBuilder {
   }
 
   async build() {
-    const creator = new User()
-    await creator.create(this.user)
+    const newUser = await this.userManagment.create(this.user)
+    if (!newUser) throw new Error('Failed Atempt to create User')
+    const wallet = await this.walletManagment.create(await this.userManagment.get.bind(this.user.email))
+    if (!wallet) throw new Error('Failed Atempt to create User\' wallet')
+
     return this.user
   }
 }
