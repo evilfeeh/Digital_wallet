9934acd7f22dcfa63ae31e7513aa4f0ae92f3cd6
evilfeeh <felipegoncalvesdsantos@gmail.com>
2024-02-19 20:36:39 -0300

    fix: add missing repository

--- src/services/Wallet.ts
+++ src/services/Wallet.ts
@@ -4,8 +4,21 @@ import { WalletRepository } from '../model/walletRepository'
 
 export class Wallet {
   walletRepository = new WalletRepository
-  user: Iuser
-  wallet: Iwallet
+  user: Iuser = {
+    id: '',
+    fullname: '',
+    CPF_CNPJ: '',
+    email: '',
+    hash: '',
+    commonUser: false,
+    active: false,
+    phone: ''
+  }
+  wallet: Iwallet = {
+    id: '',
+    user_id: '',
+    debit_amount: 0
+  }
   async create (user_id: Iuser["id"]): Promise<boolean>  {
     this.wallet.debit_amount = 0
     this.wallet.user_id = user_id
