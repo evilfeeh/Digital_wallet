4a5897d2ae7ab6da2127ce26205bfe4632459f80
felipe goncalves <felipe.goncalves@semantix.ai>
2024-02-20 17:37:29 -0300

    feat: add router to add money to account

--- src/adapters/express/server.ts
+++ src/adapters/express/server.ts
@@ -1,7 +1,7 @@
 import * as express from 'express'
 import { Request, Response, json } from 'express'
 import { UserBuilder } from '../../utils'
-import { Payment } from '../../services'
+import { Payment, Wallet, User } from '../../services'
 import { Logger } from '../../adapters/logger/logger'
 const logger = new Logger()
 const app = express();
@@ -27,9 +27,26 @@ app.post('/v1/user', async (req: Request, res: Response) => {
   }
 })
 
-app.post('/transaction', (req: Request, res: Response) => {
-  const order = req.body
+app.post('/money', async (req: Request, res: Response) => {
   try {
+    const { amount , user_email } = req.body
+    const wallet = new Wallet()
+    const user = new User()
+  
+    const user_id = (await user.get(user_email)).id;
+    const response = wallet.deposit(amount, user_id)
+    if (!response) res.status(401).json({ status: 'Error', message: "Deposit cannot be done" })
+
+    res.status(200).json({ status: 'Success', message: "Money deposited successfully", value: amount })
+  } catch (error) {
+    logger.log('error', error.message)
+    res.status(500).json({ status: 'Error', message: "Internal error server" })
+  }
+})
+
+app.post('/transaction', (req: Request, res: Response) => {
+  try {
+    const order = req.body
     const payment = new Payment(order)
     const response = payment.start()
     res.status(200).json(response)
