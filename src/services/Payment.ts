cc4cd10c6ccb5ae4956929311f8574f0fdd0d38c
felipe goncalves <felipe.goncalves@semantix.ai>
2024-02-20 17:38:57 -0300

    refactor: simplify payment transaction

--- src/services/Payment.ts
+++ src/services/Payment.ts
@@ -1,20 +1,13 @@
-import { OrderRepository, PayInRepository } from '../model'
+import { OrderRepository } from '../model'
 import { instantiateUser } from '../utils/instantiateUser'
-
-interface IOrder {
-  payer_id: string;
-  seller_id: string;
-  payment_amount: number;
-  id: string;
-  date: string;
-  status: string;
-}
+import { IOrder } from '../interfaces/order'
+import { Logger } from 'winston'
 
 export class Payment {
   private readonly order: IOrder
-  private readonly payInRepository = new PayInRepository
+  private readonly logger = new Logger
   private readonly orderRepository = new OrderRepository
-  constructor (order: any) {
+  constructor (order: IOrder) {
     this.order = order
   }
 
@@ -23,34 +16,32 @@ export class Payment {
   }
 
   async start () {
-    this.payInRepository.register(this.order),
-    this.orderRepository.save(this.order, 'OPEN_PAYMENT_ORDER') 
-
     try {
+      await this.orderRepository.save(this.order, 'STARTING TRANSACTION');
+
       const isAuthorized = await this.authorizator()
+
       if (!isAuthorized) {
-        this.orderRepository.save(this.order, 'BANK_UNATHORIZED') 
+        this.orderRepository.update(this.order, 'BANK UNATHORIZED') 
+        return { status: 'Failed', message: 'bank unathorized transaction' }
+      }
+
+      const payer = await instantiateUser(this.order.payer_email)
+      const seller = await instantiateUser(this.order.seller_email)
+
+      if (payer.debit_amount < this.order.value) {
+        this.orderRepository.update(this.order, 'BANK AUTHORIZATION FAILED NO DEBIT') 
         return { status: 'Failed', message: 'Insuficient debit amount' }
       }
 
-      const payer = await instantiateUser(this.order.payer_id)
-      const seller = await instantiateUser(this.order.seller_id)
-
-      if (payer.debit_amount < this.order.payment_amount) {
-        this.orderRepository.save(this.order, 'BANK_AUTHORIZATION_FAILED_NO_DEBIT') 
-        return { status: 'Failed', message: 'Insuficient debit amount' }
-      }
-      this.orderRepository.save(this.order, 'BANK_AUTHORIZATION_SUCCESS')
-      this.orderRepository.save(this.order, 'STARTING_PAYER_WITHDRAW')
       await payer.withdraw()
-
-      this.orderRepository.save(this.order, 'STARTING_DEPOSIT') 
       await seller.deposit()
 
-      this.orderRepository.save(this.order, 'TRANSACTION DONE SUCCESSFULLY') 
+      this.orderRepository.update(this.order, 'TRANSACTION DONE SUCCESSFULLY')
       return { status: 'Success', message: 'Order Complete Successfully' }
     } catch (error) {
-      await this.orderRepository.save(this.order, 'TRANSACTION DONE FAILED') 
+      await this.orderRepository.update(this.order, 'TRANSACTION FAILED')
+      this.logger.log('error', error.message)
       throw new Error('Order Failed')
     }
   }
