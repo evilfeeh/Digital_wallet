152f0f2ef15c6e090437d7f1a7b72a10d11a533f
felipe goncalves <felipe.goncalves@semantix.ai>
2024-02-21 19:00:47 -0300

    refactor: update migration with default message to status

--- src/config/migrations/1708463238845-orders.ts
+++ src/config/migrations/1708463238845-orders.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class Orders1708463238845 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('CREATE TABLE orders (id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(), payer_email varchar(50) NOT NULL, seller_email varchar(50) NOT NULL, value INT NOT NULL, status varchar(50))')
+        await queryRunner.query('CREATE TABLE orders (id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(), payer_email varchar(50) NOT NULL, seller_email varchar(50) NOT NULL, value INT NOT NULL, status varchar(50) DEFAULT \'STARTING TRANSACTION\')') 
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
