beb2cb9289b860c4dd6a80bce429b8cf5cdc3320
felipe goncalves <felipe.goncalves@semantix.ai>
2024-02-21 18:59:15 -0300

    feat: create order repository

--- src/model/orderRepository.ts
+++ src/model/orderRepository.ts
@@ -1,16 +1,46 @@
-import { IOrder } from '../interfaces/order'
+import datasource from '../config/ormconfig'
+import { DataSource } from 'typeorm'
+import { Orders } from '../entities'
 
-export class OrderRepository {
-  async get (order_id: string): Promise<IOrder> {
-    return
+interface IOrderRepository {
+  get(order_id: string): Promise<Orders>
+  save(order: Orders): Promise<Orders>
+  update(order: any, status: string): Promise<boolean>
+}
+
+export class OrderRepository implements IOrderRepository {
+  private AppDataSource: DataSource = datasource
+  async get (order_id: string): Promise<Orders> {
+    try {
+      return this.AppDataSource
+      .getRepository(Orders)
+      .createQueryBuilder('user')
+      .where('user.id = :order_id', { order_id })
+      .getOne()
+    } catch (error) {
+      throw new Error(error.message)
+    }
   }
-  async save (order: IOrder, status: string): Promise<IOrder> {
-    return
+  async save (order: any): Promise<Orders> {
+    try {
+      const insertedUsers = await this.AppDataSource
+      .createQueryBuilder()
+      .insert()
+      .into(Orders)
+      .values(order)
+      .execute()
+      return insertedUsers.raw
+    } catch (error) {
+      throw new Error(error.message)
+    }
   }
-  async update (order: IOrder, status: string): Promise<boolean> {
-    return
-  }
-  async delete (order: IOrder): Promise<boolean> {
-    return
+  async update (order: any, status: string): Promise<boolean> {
+    const { affected } = await this.AppDataSource
+    .createQueryBuilder()
+    .update(Orders)
+    .set(status)
+    .where("email = :order_id", { order_id: order.id })
+    .execute()
+    return (affected) ? true : false
   }
 }
