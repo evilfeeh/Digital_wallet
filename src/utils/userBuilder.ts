9ad21de573f031d4a77da00595f742d5deb875f4
evilfeeh <felipegoncalvesdsantos@gmail.com>
2024-04-23 20:56:54 -0300

    fix: error treatment

--- src/utils/userBuilder.ts
+++ src/utils/userBuilder.ts
@@ -2,7 +2,9 @@ import { hashingPassword, DataValidation } from ".";
 
 export class UserBuilder {
   private readonly dataValidation = new DataValidation();
-  user = {
+  private itHasError: boolean = false;
+  private messageErrors: string[] = [];
+  private user = {
     fullname: "",
     CPF_CNPJ: "",
     email: "",
@@ -13,47 +15,56 @@ export class UserBuilder {
   };
 
   fullname(name: string) {
-    const { status, message } = this.dataValidation.checkFullname(name);
-    if (status === "Error") throw new Error(message);
+    this.itHasError = this.dataValidation.checkFullname(name).hasError;
+    this.messageErrors.push(this.dataValidation.checkFullname(name).message);
     this.user.fullname = name;
     return this;
   }
 
   cpfCnpj(CPF_CNPJ: string) {
-    const { status, message } = this.dataValidation.checkDocument(CPF_CNPJ);
-    if (status === "Error") throw new Error(message);
+    this.itHasError = this.dataValidation.checkDocument(CPF_CNPJ).hasError;
+    this.messageErrors.push(
+      this.dataValidation.checkDocument(CPF_CNPJ).message
+    );
     this.user.CPF_CNPJ = CPF_CNPJ;
     return this;
   }
 
   password(password: string) {
-    const { status, message } = this.dataValidation.checkPassword(password);
-    if (status === "Error") throw new Error(message);
+    this.itHasError = this.dataValidation.checkPassword(password).hasError;
+    this.messageErrors.push(
+      this.dataValidation.checkPassword(password).message
+    );
+
     this.user.hash = hashingPassword(password);
     return this;
   }
 
   email(email: string) {
-    const { status, message } = this.dataValidation.checkEmail(email);
-    if (status === "Error") throw new Error(message);
+    this.itHasError = this.dataValidation.checkEmail(email).hasError;
+    this.messageErrors.push(this.dataValidation.checkEmail(email).message);
     this.user.email = email;
     return this;
   }
 
   phone(phone: string) {
-    const { status, message } = this.dataValidation.checkPhone(phone);
-    if (status === "Error") throw new Error(message);
+    this.itHasError = this.dataValidation.checkPhone(phone).hasError;
+    this.messageErrors.push(this.dataValidation.checkPhone(phone).message);
     this.user.phone = phone;
     return this;
   }
 
   commonUser() {
-    if (this.user.CPF_CNPJ.length === 11) this.user.commonUser = true;
-    this.user.commonUser = false;
+    this.user.CPF_CNPJ.length === 11
+      ? (this.user.commonUser = true)
+      : (this.user.commonUser = false);
     return this;
   }
 
-  async build() {
+  build() {
+    if (this.itHasError) {
+      throw new Error(`User Invalid: \n${this.messageErrors.join(", \n")}`);
+    }
     return this.user;
   }
 }
