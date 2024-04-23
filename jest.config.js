f86d2ee8e389c3b023ff207e5c1158decb2821d3
evilfeeh <felipegoncalvesdsantos@gmail.com>
2024-04-23 20:55:01 -0300

    chore: add properties to jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    preset: "ts-jest",
+    testEnvironment: "node",
+    clearMocks: true,
+    maxWorkers: 1,
+    testMatch: ["**/*.spec.ts"],
 };
