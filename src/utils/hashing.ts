fffb72390015d6ec4117dc4de834e761c156849f
evilfeeh <felipegoncalvesdsantos@gmail.com>
2024-04-24 00:05:10 -0300

    fix: isPasswordValid method

--- src/utils/hashing.ts
+++ src/utils/hashing.ts
@@ -1,14 +1,19 @@
-import { scryptSync, randomBytes, timingSafeEqual, BinaryLike } from 'crypto'
+import { scryptSync, randomBytes, timingSafeEqual, BinaryLike } from "crypto";
 
-export function hashingPassword (password: string) {
-  const salt = randomBytes(16).toString('hex');
-  const hashedPassword = scryptSync(password, salt, 64).toString('hex');
-  return `${salt}:${hashedPassword}`
+export function hashingPassword(password: string) {
+  const salt = randomBytes(16).toString("hex");
+  const hashedPassword = scryptSync(password, salt, 64).toString("hex");
+  return `${salt}:${hashedPassword}`;
 }
 
-export function IsPasswordValid (passwordUnderValidation: BinaryLike, correctedHash: string) {
-  const [salt, key] = correctedHash.split(':');
-  const hashedPassword = Buffer.from(scryptSync(passwordUnderValidation, salt, 64).toString('hex'));
-  const keyBuffer = Buffer.from(key, 'hex');
+export function IsPasswordValid(
+  passwordUnderValidation: BinaryLike,
+  correctedHash: string
+) {
+  const [salt, key] = correctedHash.split(":");
+  const hashedPassword = Buffer.from(
+    scryptSync(passwordUnderValidation, salt, 64).toString("hex")
+  );
+  const keyBuffer = Buffer.from(key);
   return timingSafeEqual(keyBuffer, hashedPassword);
 }
