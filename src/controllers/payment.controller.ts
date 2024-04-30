5becdebdb7a7a35eeb72a2bffe3c4ac43ca3a280
evilfeeh <felipegoncalvesdsantos@gmail.com>
2024-04-30 19:56:43 -0300

    feat: introducing transaction

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -1,11 +1,12 @@
-import { OrderRepository } from "../model";
-import { InstantiateUser } from "../utils/instantiateUser";
+import { OrderRepository, UserRepository, WalletRepository } from "../model";
 import { IOrder } from "../interfaces/order";
 import { Logger } from "../adapters/logger/logger";
 
 export class Payment {
   private readonly logger = new Logger();
   private readonly orderRepository = new OrderRepository();
+  private readonly userRepository = new UserRepository();
+  private readonly walletRepository = new WalletRepository();
 
   private async authorizator(): Promise<boolean> {
     const randomNumber = Math.floor(Math.random() * 10);
@@ -25,10 +26,17 @@ export class Payment {
         return { status: "Failed", message: "bank unathorized transaction" };
       }
 
-      const payer = new InstantiateUser(order.payer_email);
-      const seller = new InstantiateUser(order.seller_email);
+      const [payer, seller] = await Promise.all([
+        this.userRepository.get(order.payer_email),
+        this.userRepository.get(order.seller_email),
+      ]);
 
-      if (payer.total_amount < order.value) {
+      const [payer_wallet, seller_wallet] = await Promise.all([
+        this.walletRepository.get(payer.id),
+        this.walletRepository.get(seller.id),
+      ]);
+
+      if (payer_wallet.debit_amount < order.value) {
         this.orderRepository.update(
           insertedOrderId,
           "BANK AUTHORIZATION FAILED - NO DEBIT"
@@ -36,8 +44,17 @@ export class Payment {
         return { status: "Failed", message: "Insuficient debit amount" };
       }
 
-      await payer.withdraw(order.value);
-      await seller.deposit(order.value);
+      await this.orderRepository.transaction(
+        {
+          user_id: payer.id,
+          debit_amount: payer_wallet.debit_amount,
+        },
+        {
+          user_id: seller.id,
+          debit_amount: seller_wallet.debit_amount,
+        },
+        order.value
+      );
 
       this.orderRepository.update(
         insertedOrderId,
--- src/model/orderRepository.ts
+++ src/model/orderRepository.ts
@@ -1,11 +1,16 @@
 import datasource from "../config/ormconfig";
 import { DataSource } from "typeorm";
-import { Orders } from "../entities";
+import { Orders, Wallet } from "../entities";
 
 interface IOrderRepository {
   get(order_id: string): Promise<Orders>;
   save(order: Orders): Promise<Orders["id"]>;
   update(order: any, status: string): Promise<boolean>;
+}
+
+interface UserTransaction {
+  debit_amount: number;
+  user_id: string;
 }
 
 export class OrderRepository implements IOrderRepository {
@@ -40,6 +45,32 @@ export class OrderRepository implements IOrderRepository {
         .where("id = :order_id", { order_id: order_id })
         .execute();
       return affected ? true : false;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async transaction(
+    payer: UserTransaction,
+    seller: UserTransaction,
+    value: number
+  ) {
+    try {
+      await this.AppDataSource.transaction(
+        async (transactionalEntityManager) => {
+          await transactionalEntityManager
+            .createQueryBuilder()
+            .update(Wallet)
+            .set({ debit_amount: payer.debit_amount - value })
+            .where("user_id = :user_id", { user_id: payer.user_id })
+            .execute();
+          await transactionalEntityManager
+            .createQueryBuilder()
+            .update(Wallet)
+            .set({ debit_amount: seller.debit_amount + value })
+            .where("user_id = :user_id", { user_id: seller.user_id })
+            .execute();
+        }
+      );
     } catch (error) {
       throw error;
     }
