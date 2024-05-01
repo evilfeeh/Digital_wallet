e563a4bd689c06d820b1d0414b79b141c468fe07
evilfeeh <felipegoncalvesdsantos@gmail.com>
2024-04-30 21:17:59 -0300

    feat: implement middleware

--- src/adapters/express/server.ts
+++ src/adapters/express/server.ts
@@ -6,19 +6,23 @@ import walletRouter from "./routers/wallet.router";
 import loginRouter from "./routers/login.router";
 import * as swaggerUi from "swagger-ui-express";
 import { swaggerDocument } from "../swagger/config";
+import { authMiddleware } from "./middlewares";
 
 const app = express();
 
 app.use(json());
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use("/v1", userRouter);
-app.use("/v1", loginRouter);
-app.use("/v1", transactionRouter);
-app.use("/v1", walletRouter);
-
 app.get("/v1/ping", (req: Request, res: Response) => {
   res.send("pong");
 });
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/v1", loginRouter);
+
+app.use(authMiddleware);
+
+app.use("/v1", userRouter);
+app.use("/v1", transactionRouter);
+app.use("/v1", walletRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening into ${process.env.PORT}`);
