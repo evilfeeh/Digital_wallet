1195bf1073f8ec798362bb693a5a259bc5108cad
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2024-06-10 23:43:21 -0300

    refactor: typemorm entity

--- src/adapters/outbound/database/entities/order.ts
+++ src/adapters/outbound/database/entities/order.ts
@@ -1,19 +1,21 @@
-import { IOrder } from "../../application/entities/Iorder";
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity()
-export class Orders implements IOrder {
+export class Orders {
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
   @Column("varchar", { length: 50 })
-  payer_email: string;
+  payer_id: string;
 
   @Column("varchar", { length: 50 })
-  seller_email: string;
+  seller_id: string;
 
   @Column("float", { precision: 10, scale: 2 })
   value: number;
+
+  @Column("float", { precision: 10, scale: 2 })
+  fee: number;
 
   @Column("varchar", { length: 30 })
   status: string;
