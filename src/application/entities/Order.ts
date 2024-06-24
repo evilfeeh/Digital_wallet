92213c66f90a60d7bfc6765092c4d1010836b242
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2024-06-24 20:56:36 -0300

    refactor: define methods to public

--- src/application/entities/Order.ts
+++ src/application/entities/Order.ts
@@ -18,27 +18,31 @@ export class Order extends Entity<Iorder> {
     super(properties, id);
   }
 
-  get id() {
-    return this.id;
+  public get id() {
+    return this._id;
   }
 
-  get value() {
+  public get value() {
     return this.properties.value;
   }
 
-  get status() {
+  public get status() {
     return this.properties.status;
   }
 
-  get payer_id() {
+  public get payer_id() {
     return this.properties.payer_id;
   }
 
-  get seller_id() {
+  public get seller_id() {
     return this.properties.seller_id;
   }
 
-  defineFee() {}
+  public get fee() {
+    return this.properties.fee;
+  }
 
-  public create(properties: Iorder, id?: UniqueEntityID) {}
+  public static create(properties: Iorder, id?: UniqueEntityID) {
+    return new Order(properties, id);
+  }
 }
